934c7bab07477ed6823b2a11531a1d7b7167934a
bejaouij <bejaoui.je@mail.com>
2019-01-15 05:26:33 +0100

    feat($view): create the device add form modal
    Create the device add form modal on the user dashboard.

--- resources/views/device.blade.php
+++ resources/views/device.blade.php
@@ -47,11 +47,49 @@ function buttonTypeByStatusCode($statusCode) : String {
 			</div>
 		</div>
 	</div>
-
-	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-        <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#exampleModal">Ajouter un dispositif</button>
-    </div>
 	@empty
 	Vous n'avez pas encore enregistré de dispositif.
 	@endforelse
+
+	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+		<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#add-device-form-container">Ajouter un dispositif</button>
+	</div>
+
+	<div class="modal fade" id="add-device-form-container" tabindex="-1" role="dialog" aria-labelledby="add-device-form-container-label" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="add-device-form-container-label">Ajouter un dispositif</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+
+				<div class="modal-body">
+					<form id="add-device-form" method="POST" action="#">
+						@csrf
+						<div class="form-group row">
+							<label for="device_serial_number_part_1" class="col-sm-2 col-form-label">Clef*</label>
+							<div class="col-sm-10 d-flex">
+								<input class="form-control col-sm-3" id="device_serial_number_part_1" name="device_serial_number_part_1" minlength="4" maxlength="4"><pre> </pre>
+								<input class="form-control col-sm-3" id="device_serial_number_part_2" name="device_serial_number_part_2" minlength="4" maxlength="4"><pre> </pre>
+								<input class="form-control col-sm-3" id="device_serial_number_part_3" name="device_serial_number_part_3" minlength="4" maxlength="4">
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="nom" class="col-sm-2 col-form-label">Nom*</label>
+							<div class="col-sm-10">
+								<input class="form-control" id="device_name" name="device_name" placeholder="Nom du dispositif" minlength="1" maxlength="50">
+							</div>
+						</div>
+					</form>
+				</div>
+
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+					<button type="button" class="btn btn-primary" onclick="event.preventDefault(); document.getElementById('add-device-form').submit()">Enregistrer</button>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
