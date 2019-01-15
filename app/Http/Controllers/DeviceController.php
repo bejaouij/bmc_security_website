aa122d6381a9a172a1413eafe6a8cbf0e5a1cc5d
bejaouij <bejaoui.je@mail.com>
2019-01-15 04:59:08 +0100

    feat($controller): implement the method to associate a vehicule to a device
    Implement the 'device' Controller method to allow user to asociate a vehicle to a specific device.
    
    Before: just die and dump request data in the controller.
    After:
    - Check wether both device and vehicle exist and belong to the current user.
    - If it is the case, associate the vehicle to the device.

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Device;
+use App\Vehicle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DeviceController extends Controller
 {
-    public function associateVehicle(Request $request) {
-    	dd($request);
+    public function associateVehicle(int $device_id, int $vehicle_id) {
+    	$vehicle = Vehicle::findOrFail($vehicle_id);
 
-    	return redirest()->back()->with('success', 'Véhicule associé au dispositif avec succès.');
+    	if($vehicle->user_id != Auth::user()->user_id)
+    	    abort(404);
+
+        $device = Device::findOrFail($device_id);
+
+        if($device->user_id != Auth::user()->user_id)
+            abort(404);
+
+        $device->vehicle_id = $vehicle_id;
+        $device->save();
+
+    	return redirect()->back()->with('success', 'Véhicule associé au dispositif avec succès.');
     }
 }
