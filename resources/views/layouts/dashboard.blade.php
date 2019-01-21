11fe3e9352b1a94c5c7e2d274457fa6c3f6d829e
bejaouij <bejaoui.je@mail.com>
2019-01-21 12:32:52 +0100

    style($view): remove dashboard searchbar
    Remove the dashboard searchbar.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -15,7 +15,6 @@
     <body>
         <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 	        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('dashboard') }}">BMC Security</a>
-	        <input class="form-control form-control-dark w-100" type="text" placeholder="Rechercher" aria-label="Search">
 	        <ul class="navbar-nav px-3">
 	            <a class="nav-link" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
